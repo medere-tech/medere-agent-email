af884e578e8543cf38e7a7b9eea0c535aaa70f69
dethiefaye <153307843+dethiefaye@users.noreply.github.com>
2026-03-05 16:23:49 +0100

    Revert "Update AgentMail.tsx"
    This reverts commit 6d969de09f2d45c8726dd016690f5dc1c8d465e7.

--- components/AgentMail.tsx
+++ components/AgentMail.tsx
@@ -763,7 +763,7 @@ export default function AgentMail() {
 
         {/* Footer */}
         <div className="mt-4 text-center text-xs text-zinc-400">
-          Créé par <a href="https://www.linkedin.com/in/faye-dethie-responsable-seo-junior/" target="_blank" rel="noopener noreferrer" className="hover:underline">Dethie</a> | Copyright © 2026 Médéré · Tous droits réservés
+          Créé par <a href="https://www.linkedin.com/in/faye-dethie-responsable-seo-junior/" target="_blank" rel="noopener noreferrer" className="hover:underline">Dethie</a> | Copyright © 2025 Médéré · Tous droits réservés
         </div>
       </div>
     </div>
